ba41ba9566931936c3a0f1e493567854f64b7cc4
rainer.diekmann <rainer.diekmann@hamburg.de>
2023-04-25 17:13:15 +0200

    Manual deploy to fly.io

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # spring-boot-flyio
+
 Deploy Springboot application on fly.io
+
+mvn clean spring-boot:build-image
+
+fly launch --image docker.io/library/demo:0.0.1-SNAPSHOT
+
+fly deploy --local-only --image demo:0.0.1-SNAPSHOT
---
+++ src/main/java/com/diekmann/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.diekmann.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
+
+  @GetMapping("/hello")
+  public ResponseEntity hello() {
+    return new ResponseEntity<>("hello from fly", HttpStatus.OK);
+  }
+
+}
